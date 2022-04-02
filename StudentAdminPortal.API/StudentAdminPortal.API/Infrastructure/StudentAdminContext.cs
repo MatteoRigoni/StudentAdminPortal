f7d594dfb4426e571c61506f6016783e9e1e770f
MatteoRigoni <matteo.rigoni2@gmail.com>
2022-04-02 15:02:57 +0200

    Added database

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Infrastructure/StudentAdminContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAdminPortal.API.Entities;
+
+namespace StudentAdminPortal.API.Infrastructure
+{
+    public class StudentAdminContext : DbContext
+    {
+        public StudentAdminContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Student> Addresses { get; set; }
+        public DbSet<Student> Genders { get; set; }
+    }
+}
